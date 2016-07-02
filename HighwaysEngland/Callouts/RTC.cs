61e8fdfa50bc279c0fbcca181e784df613d80701
Maurice Moss <cammygamesyt@gmail.com>
2016-07-02 09:47:39 +0100

    Added some function to common which could be used in other callouts.

--- HighwaysEngland/Callouts/RTC.cs
+++ HighwaysEngland/Callouts/RTC.cs
@@ -21,22 +21,14 @@ namespace HighwaysEngland.Callouts
         private Vehicle vehicle2;
         private Blip blip;
         private Vector3 spawnPoint;
-        private int veh1;
-        private int veh2;
         private CalloutState state;
-        private Random random;
 
-        private String[] vehicles = { "utillitruck2", "voodoo2", "baller2", "baller", "bison2", "bison3", "burrito", "pony2", "rapidgt2", "scrap", "minivan", "ninef2", "surfer", "taco" };
 
         public override bool OnBeforeCalloutDisplayed()
         {
             spawnPoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(500f));
             ShowCalloutAreaBlipBeforeAccepting(spawnPoint, 30f);
             AddMinimumDistanceCheck(250f, spawnPoint);
-
-            random = new Random();
-            veh1 = random.Next(vehicles.Length);
-            veh2 = random.Next(vehicles.Length);
 
             CalloutMessage = "~o~Road Traffic Collision";
             CalloutPosition = spawnPoint;
@@ -51,18 +43,12 @@ namespace HighwaysEngland.Callouts
 
             state = CalloutState.EnRoute;
 
-            Vector3 closestVehicleNodeCoords;
-            float roadheading;
-
-            NativeFunction.Natives.GET_CLOSEST_VEHICLE_NODE_WITH_HEADING(spawnPoint.X, spawnPoint.Y, spawnPoint.Z, out closestVehicleNodeCoords, out roadheading, 1, 3, 0);
-            if (roadheading.Equals(null)) { roadheading = 180f; }
-            vehicle1 = new Vehicle(vehicles[veh1], spawnPoint, roadheading);
+            vehicle1 = new Vehicle(vehicles[rand.Next(vehicles.Length)], spawnPoint, getTrafficHeading(spawnPoint));
             setUpVeh(vehicle1, 55f, 40f, true, false); 
             vehicle1.Health = 40;
-            if (vehicle1.Doors[4].IsValid()) vehicle1.Doors[4].Open(false);
+            openVehilceDoor(vehicle1, 4, false);
 
-
-            vehicle2 = new Vehicle(vehicles[veh2], vehicle1.GetOffsetPositionFront(5f), vehicle1.Heading);
+            vehicle2 = new Vehicle(vehicles[rand.Next(vehicles.Length)], vehicle1.GetOffsetPositionFront(5f), vehicle1.Heading);
             setUpVeh(vehicle2, 200f, 100f, true, false);
 
             driver1 = vehicle1.CreateRandomDriver();
@@ -87,7 +73,7 @@ namespace HighwaysEngland.Callouts
             }
 
 
-            if (!Functions.IsCalloutRunning() && state == CalloutState.Complete) End();
+            if (!Functions.IsCalloutRunning()) End();
         }
 
         public override void End()
@@ -111,8 +97,7 @@ namespace HighwaysEngland.Callouts
 
         private void setupPed(Ped ped, Vehicle vehicle)
         {
-            Persona persona = Functions.GetPersonaForPed(ped);
-            Functions.SetVehicleOwnerName(vehicle, persona.FullName);
+            linkPedToVehicle(ped, vehicle);
             ped.Tasks.LeaveVehicle(LeaveVehicleFlags.LeaveDoorOpen).WaitForCompletion(6000);
             ped.IsPersistent = true;
             ped.BlockPermanentEvents = true;
@@ -126,7 +111,7 @@ namespace HighwaysEngland.Callouts
                 GameFiber.Sleep(4500);
                 driver2.PlayAmbientSpeech("GENERIC_CURSE_HIGH", true);
 
-                if (random.Next(5) >= 4)
+                if (rand.Next(5) >= 4)
                 {
                     driver2.Tasks.FightAgainst(driver1);
                     driver1.Tasks.FightAgainst(driver2);
--- HighwaysEngland/Util/Common.cs
+++ HighwaysEngland/Util/Common.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Engine.Scripting.Entities;
 
 namespace HighwaysEngland.Util
 {
-    class Common
+    public class Common
     {
-        public Random rand = new Random();
+        public static Random rand = new Random();
 
         public enum CalloutState
         {
@@ -17,5 +17,32 @@ namespace HighwaysEngland.Util
             Arrived,
             Complete
         }
+
+        public static String[] vehicles = { "utillitruck2", "voodoo2", "baller2", "baller", "bison2", "bison3", "burrito", "pony2", "rapidgt2", "scrap", "minivan", "ninef2", "surfer", "taco" };
+
+        /// <summary>Returns the heading of nearby traffic</summary>
+        /// <param name="position"> Position to check</param>    
+        public static float getTrafficHeading(Vector3 position)
+        {
+            float trafficHeading;
+            Vector3 closestVehicleNodeCoords;
+
+            NativeFunction.Natives.GET_CLOSEST_VEHICLE_NODE_WITH_HEADING(position.X, position.Y, position.Z, out closestVehicleNodeCoords, out trafficHeading, 1, 3, 0);
+            if (trafficHeading.Equals(null)) { trafficHeading = 90f; }
+            return trafficHeading;
+        }
+
+        /// <summary>Sets a vehicles owner name to match peds full name</summary>   
+        public static void linkPedToVehicle(Ped ped, Vehicle vehicle)
+        {
+            Persona persona = Functions.GetPersonaForPed(ped);
+            Functions.SetVehicleOwnerName(vehicle, persona.FullName);
+        }
+
+        public static void openVehilceDoor(Vehicle vehicle, int index, bool instant)
+        {
+            if (vehicle.Doors[index].IsValid()) vehicle.Doors[index].Open(instant);
+        }
+
     }
 }
